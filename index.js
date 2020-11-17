2d081566726c0a99354cb7ccbc42fec0d359853d
huyhomie66 <huyhomie66@gmail.com>
2020-11-18 03:44:51 +0700

    dkm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { name as appName } from './app.json';
 
 import { SENTRY_TOKEN_URL } from './sentry';
 
-import App from './src/app';
+import App from './App';
 
 AppRegistry.registerComponent(appName, () => App);
 
